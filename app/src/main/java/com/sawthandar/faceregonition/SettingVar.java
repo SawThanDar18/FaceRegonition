bc6518cab3c1d3aaa56dffc34aebf530c6058715
SawThanDar18 <sawthandar1998.pw@gmail.com>
2023-08-29 16:33:32 +0630

    adding UI for project

---
+++ app/src/main/java/com/sawthandar/faceregonition/SettingVar.java
@@ -0,0 +1,17 @@
+package com.sawthandar.faceregonition;
+
+import mcv.facepass.types.FacePassImageRotation;
+
+public class SettingVar {
+    public static boolean cameraFacingFront = false;
+    public static int faceRotation = FacePassImageRotation.DEG270;
+    public static boolean isSettingAvailable = true;
+    public static int cameraPreviewRotation = 90;
+    public static boolean isCross = false;
+    public static String SharedPrefrence = "user";
+    public static int mHeight;
+    public static int mWidth;
+    public static boolean cameraSettingOk = false;
+    public static boolean iscameraNeedConfig = false;
+    public static boolean isButtonInvisible = false;
+}
